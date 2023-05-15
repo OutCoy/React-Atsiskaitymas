90a93986c2606ac474cb32c3eb2d7631df801a54
OutCoy <cizikasg@gmail.com>
2023-05-15 11:44:31 +0300

    Added some routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Molecules/Header';
 import Footer from './components/Molecules/Footer';
+import { useContext } from 'react';
+import UsersContext from './contexts/UsersContext';
+import Home from './components/Pages/Home';
+import Register from './components/Pages/Register';
+import Login from './components/Pages/Login';
 
 const App = () => {
+
+  const {currentUser} = useContext(UsersContext);
+  console.log(currentUser);
+
   return (
     <>
       <Header />
       <Routes>
-        <Route path='/' element={''}/>
+        <Route  path='/register' element={<Register />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/' element={currentUser ? <Home /> : <Navigate to='/login' />}/>
       </Routes>
       <Footer />
     </>
