a2848133f7a6d56fdfde46a556ed3e50586fdc8b
OutCoy <cizikasg@gmail.com>
2023-05-15 16:42:07 +0300

    Files formatting changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Header from './components/Molecules/Header';
-import Footer from './components/Molecules/Footer';
-import { useContext } from 'react';
-import UsersContext from './contexts/UsersContext';
-import Home from './components/Pages/Home';
-import Register from './components/Pages/Register';
-import Login from './components/Pages/Login';
-import NewPlant from './components/Pages/NewPlant';
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Molecules/Header";
+import Footer from "./components/Molecules/Footer";
+import { useContext } from "react";
+import UsersContext from "./contexts/UsersContext";
+import Home from "./components/Pages/Home";
+import Register from "./components/Pages/Register";
+import Login from "./components/Pages/Login";
+import NewPlant from "./components/Pages/NewPlant";
 
 const App = () => {
-
-  const {currentUser} = useContext(UsersContext);
+  const { currentUser } = useContext(UsersContext);
   console.log(currentUser);
 
   return (
     <>
       <Header />
       <Routes>
-        <Route  path='/register' element={<Register />}/>
-        <Route path='/login' element={<Login />}/>
-        <Route path='/' element={currentUser ? <Home /> : <Navigate to='/login' />}/>
-        <Route path='/addNewPlant' element={currentUser ? <NewPlant /> : <Navigate to='/login' />}/>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={currentUser ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/addNewPlant"
+          element={currentUser ? <NewPlant /> : <Navigate to="/login" />}
+        />
       </Routes>
       <Footer />
     </>
   );
-}
+};
 
 export default App;
