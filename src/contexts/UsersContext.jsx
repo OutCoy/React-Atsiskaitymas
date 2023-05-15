d8a2985c7eb3ac6fc3192ed46b9d6fd4ce983cca
OutCoy <cizikasg@gmail.com>
2023-05-15 11:43:25 +0300

    Added additional (add) functionality to users

--- src/contexts/UsersContext.jsx
+++ src/contexts/UsersContext.jsx
@@ -3,13 +3,23 @@ import { createContext, useEffect, useReducer, useState } from "react";
 const UsersContext = createContext(null);
 
 const UsersActionTypes = {
-  get: 'get_all_users'
+  get: 'get_all_users',
+  add: 'add_new_user'
 }
 
 const reducer = (state, action) => {
   switch(action.type){
     case UsersActionTypes.get:
       return action.data;
+    case UsersActionTypes.add:
+      fetch('http://localhost:8080/users', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(action.data)
+      })
+      return [...state, action.data];
     default:
       return state;
   }
@@ -35,7 +45,8 @@ const UsersProvider = ({children}) => {
         currentUser,
         users,
         setUsers,
-        setCurrentUser
+        setCurrentUser,
+        UsersActionTypes
       }}
     >
       {children}
